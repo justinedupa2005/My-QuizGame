692e3e68833a87ada168ddb3f7f33372bc23299b
justinedupa2005 <justinedupa2005@gmail.com>
2025-10-23 22:29:50 +0800

    Added signUp Functionality and database

--- JS/firebaseAuth.js
+++ JS/firebaseAuth.js
@@ -13,5 +13,57 @@ const firebaseConfig = {
     appId: "1:738834776023:web:c4581e984670a315e09592"
 };
 
+function showMessage(message, type) {
+  const messageBox = document.getElementById("messageBox");
+  messageBox.textContent = message;
+  
+  // Remove old classes and apply the new one
+  messageBox.className = type; 
+  messageBox.style.display = "block";
+  
+  // Hide after 3 seconds
+  setTimeout(() => {
+    messageBox.style.display = "none";
+  }, 3000);
+}
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+const signUp = document.getElementById("signUpBtn");
+signUp.addEventListener('click', async (event)=>{
+    event.preventDefault();
+    const username = document.getElementById("rUserName").value;
+    const email = document.getElementById("rEmail").value;
+    const password = document.getElementById("rPassword").value;
+    const confirmPassword = document.getElementById("rConfirmPassword").value;
+
+    if(password !== confirmPassword){
+        showMessage("Passwords do not match!");
+        return;
+    }
+
+    try{
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+
+        // Save to firebase
+        await setDoc(doc(db, "users", user.uid), {
+            username: username,
+            email: email
+        })
+
+        showMessage("Account Created Successfully!");
+        window.location.href = "login.html";
+    }
+    catch (error) {
+        if (error.code === "auth/email-already-in-use") {
+            showMessage("Email Address Already Exists!");
+        } else {
+            showMessage("Unable to create user: " + error.message);
+        }
+        console.error("Error:", error);
+    }
+})
